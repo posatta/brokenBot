43716ae095515149d49158f1c6098602c3edd6c8
brokenVectors <brokenvectors@gmail.com>
2020-08-30 23:09:19 +0200

    i did a few more things

--- main.js
+++ main.js
@@ -18,6 +18,20 @@ let commands = {
   say: function(msg, args){
     msg.channel.send(args.join(" "))
   },
+  canned: function(msg, args){
+    if(args[0] == 1){
+      msg.channel.send("Thanks for letting us know. We're looking into it.")
+    }
+    else if(args[0] == 2){
+      msg.channel.send("Thanks for letting us know. We agree there is an issue and we're looking into it.")
+    }
+    else if(args[0] == 3){
+      msg.channel.send("Thanks for letting us know. We agree there is an issue and we've removed the post.")
+    }
+    else{
+      msg.channel.send("That canned response isn't in my database, try a number between 1 and 3.")
+    }
+  },
   help: function(msg, args){
     msg.channel.send(`
     \`\`\`
@@ -25,10 +39,7 @@ ping: make the bot reply with pong
 epic_phrase: make the bot reply with an epic phrase
 say: make the bot say something. if you provide LUA as first argument, xapacni will delete your message and only the bot's message will be visible.
 help: it's this noob
-
-    \`\`\`
-    
-    `)
+    \`\`\``)
   }
 }
 
