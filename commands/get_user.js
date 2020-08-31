a0b79851739838340097b9b13bc3879bfb04541f
brokenVectors <brokenvectors@gmail.com>
2020-08-31 21:09:58 +0200

    fixed issue where code after fetch would run before, resulting in userData being empty

--- commands/get_user.js
+++ commands/get_user.js
@@ -1,9 +1,19 @@
 const fetch = require("node-fetch");
 
 module.exports = function(msg, args, Discord){
+  let url = `https://devforum.roblox.com/u/${args[0]}/summary`
+  let profilePicture = `https://doy2mn9upadnk.cloudfront.net/user_avatar/devforum.roblox.com/${args[0]}/120/1144704_2.png`
 
-    let userData = {}
-    fetch(`https://devforum.roblox.com/u/${args[0]}.json`)
+  const exampleEmbed = new Discord.MessageEmbed()
+  .setColor('#0099ff')
+  .setTitle(args[0])
+  .setURL(url)
+  .setAuthor(args[0], profilePicture, url)
+  .setThumbnail(profilePicture)
+  .setImage(profilePicture)
+
+  
+  fetch(`https://devforum.roblox.com/u/${args[0]}.json`)
     .then(
       function(response) {
         if (response.status !== 200) {
@@ -14,7 +24,7 @@ module.exports = function(msg, args, Discord){
         // Examine the text in the response
         response.json().then(function(data) {
           console.log(data.user.bio_excerpt)
-          userData = data;
+          exampleEmbed.setDescription(data.user.bio_excerpt)
         });
       }
     )
@@ -24,25 +34,20 @@ module.exports = function(msg, args, Discord){
     });
 
 
-    let url = `https://devforum.roblox.com/u/${args[0]}/summary`
-    let profilePicture = `https://doy2mn9upadnk.cloudfront.net/user_avatar/devforum.roblox.com/${args[0]}/120/1144704_2.png`
-    const exampleEmbed = new Discord.MessageEmbed()
-      .setColor('#0099ff')
-      .setTitle(args[0])
-      .setURL(url)
-      .setAuthor(args[0], profilePicture, url)
-      .setDescription("bio could not be loaded")
-      .setThumbnail(profilePicture)
+    
+    
+      
+      
+      /*
       .addFields(
         { name: 'Regular field title', value: 'Some value here' },
         { name: '\u200B', value: '\u200B' },
         { name: 'Inline field title', value: 'Some value here', inline: true },
         { name: 'Inline field title', value: 'Some value here', inline: true },
       )
-      .addField('Inline field title', 'Some value here', true)
-      .setImage(profilePicture)
-      .setTimestamp()
-      .setFooter('Some footer text here', profilePicture);
+      */
+      
+      
 
     msg.channel.send(exampleEmbed);
   }
