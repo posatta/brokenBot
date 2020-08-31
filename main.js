022b9200d924b83404faa20e9211c39313e1f7aa
brokenVectors <brokenvectors@gmail.com>
2020-08-31 12:28:53 +0200

    added get_tl command to help

--- main.js
+++ main.js
@@ -58,6 +58,7 @@ ping: make the bot reply with pong
 epic_phrase: make the bot reply with an epic phrase
 say: make the bot say something. if you provide LUA as first argument, xapacni will delete your message and only the bot's message will be visible.
 help: it's this noob
+get_tl: get TL of a user
     \`\`\``)
   }
 }
