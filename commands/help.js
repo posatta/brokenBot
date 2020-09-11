71415cd2fd4db5228e4a4dab788cf317dad0e525
brokenVectors <brokenvectors@gmail.com>
2020-09-11 20:20:18 +0200

    help updated

--- commands/help.js
+++ commands/help.js
@@ -6,5 +6,8 @@ epic_phrase: make the bot reply with an epic phrase
 say: make the bot say something. if you provide LUA as first argument, xapacni will delete your message and only the bot's message will be visible.
 help: it's this noob
 get_tl: get TL of a user
+get_user: view some spicy info of a user
+8ball: bot will respond with random funny
+kick: kick a user. don't try this if u dont have perms u noob
     \`\`\``)
   }
